e3f4a026deeb4f9e3e0b2ebea04734a9716cb018
VenkateshJB0906 <kuttyvenkat0906@gmail.com>
2025-04-18 21:19:30 +0530

    Update App.js

--- socket/src/App.js
+++ socket/src/App.js
@@ -1,7 +1,7 @@
 // import {io} from "socket.io";
 import { io } from 'socket.io-client';
 import Main from "./components/Main";
-const socket=io("http://localhost:4000/")
+const socket=io(process.env.ser)
 
 function App() { 
   return (
